9830e33d3d7b1a0882f78931fad1514a557b99ea
Sonakshi1901 <sonakshi1901@gmail.com>
2021-02-23 15:41:04 +0400

    Update SignIn.js

--- photoblog/src/components/Register/SignIn/SignIn.js
+++ photoblog/src/components/Register/SignIn/SignIn.js
@@ -15,19 +15,22 @@ const SignIn = (props) => {
         <span className="SignIn__logo3"></span>
 
         <div className="SignIn__username">
-         <span className="SignIn__name">Enter your username</span>
+         <input type="text" placeholder="Enter your username"/>
          <span>{props.name}</span>
         </div>
 
-        <span className="SignIn__next1"></span>
+        <button className="SignIn__next1"></button>
         <span className="SignIn__next2"></span>
         <span className="SignIn__next3"></span>
 
 
         <div className="SignIn__password">
-         <span className="SignIn__user__password">Enter your password</span>
+         <input type="password" placeholder="Enter your password"/>
          <span>{props.password}</span>
+         <a href="##" className="SignIn__forgotpass">Forgot Password?</a>
         </div>
+
+        <a href="##" className="SignUp__change">Wanna Sign Up?</a>
 
       </div>
     </div>
