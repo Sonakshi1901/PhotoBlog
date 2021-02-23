651ba7333eb02570f66bbf11c5d65d9d31fbe44c
Sonakshi1901 <sonakshi1901@gmail.com>
2021-02-23 12:02:21 +0400

    Update Home.js

--- photoblog/src/components/Home/Home.js
+++ photoblog/src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
-import PhotoCard from "../Post/PhotoCard"
+import PhotoCard from "../Post/PhotoCard";
+import SignIn from "../Register/SignIn/SignIn";
 
 const Home = () => {
 
@@ -9,18 +10,8 @@ const Home = () => {
       <div className="home__header">
         <h2>PhotoBlog</h2>
           <div className="home__post">
-            <PhotoCard
-             name="Username"
-             caption="This is soo cool!!!"
-             date="FEB 10, 2021"/>
-            <PhotoCard
-              name="Username2"
-              caption="This is soo cool!!!"
-              date="FEB 18, 2021"/>
-              <PhotoCard
-                name="Username3"
-                caption="This is soo cool!!!"
-                date="FEB 15, 2021"/>
+            <SignIn/>
+
           </div>
       </div>
     </div>
