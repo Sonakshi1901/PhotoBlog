aa4fab8175906ef06e38c38c7d0dda9c2c1cf2cd
Sonakshi1901 <sonakshi1901@gmail.com>
2021-05-10 21:36:22 +0400

    models

---
+++ server/models/postMessage.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+{/*mongoose gives us a uniformity in our documents*/}
+const postSchema = mongoose.Schema({
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: new Date()
+  },
+});
+
+const PostMessage = mongoose.model("PostMessage", postSchema);
+
+export default PostMessage;
