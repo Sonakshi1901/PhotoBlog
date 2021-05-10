4f398782555f1cb3cdc5c41d35b1666f439dbe6a
Sonakshi1901 <sonakshi1901@gmail.com>
2021-05-10 23:59:34 +0400

    Updated homepage

--- photoblog/src/App.js
+++ photoblog/src/App.js
@@ -1,12 +1,30 @@
 import React from "react";
-import './App.css';
 import Home from "./components/Home/Home";
+import Posts from "./components/Posts/Posts.js";
+import Form from "./components/Forms/Form.js";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import useStyles from "./styles";
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-       <Home/>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+         <Typography className={classes.heading} variant="h3" align="center">Polaroid</Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+           <Grid container justify="Space-between" alignItems="stretch" spacing={3}>
+               <Grid item xs={12} sm={7}>
+                    <Posts />
+               </Grid>
+               <Grid item xs={12} sm={4}>
+                    <Form />
+               </Grid>
+           </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
