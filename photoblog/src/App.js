486e44e97558f21a7ffd15345b5940c56e306568
Sonakshi1901 <sonakshi1901@gmail.com>
2021-05-16 23:59:14 +0400

    Update App.js

--- photoblog/src/App.js
+++ photoblog/src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./components/Home/Home";
 import Posts from "./components/Posts/Posts.js";
 import Form from "./components/Forms/Form.js";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import useStyles from "./styles";
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts';
 
 function App() {
+  const dispatch = useDispatch();
   const classes = useStyles();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
     <Container maxwidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
