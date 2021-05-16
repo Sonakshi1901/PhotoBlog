b8bc975b33489ef811eb781f06e80cd318b86486
Sonakshi1901 <sonakshi1901@gmail.com>
2021-05-16 23:58:15 +0400

    Update Posts.js

--- photoblog/src/components/Posts/Posts.js
+++ photoblog/src/components/Posts/Posts.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { useSelector } from 'react-redux';
 import Post from "./Post/Post.js";
 import useStyles from "./styles";
 
 const Posts = () => {
+  {/*In combineReducers we are exporting 'posts' so we write state.posts*/}
+  const posts = useSelector((state) => state.posts);
   const classes = useStyles();
-  
+
+  console.log(posts);
+
   return (
     <>
       <Post />
